1b288ad42e131cc038988ab0b3a08299962f6f62
minki1220 <130380152+minki1220@users.noreply.github.com>
2025-12-08 04:41:54 +0900

    Update useMe.tsx
    Type error: Property 'profileImageUrl' does not exist on type '{}'

--- src/lib/user/useMe.tsx
+++ src/lib/user/useMe.tsx
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchMe } from "./userApi";
+import { fetchMe, UserInfoResponse } from "./userApi";
+
+type MeData = UserInfoResponse["data"];
 
 const useMe = () => {
-    return useQuery({
-        queryKey: ["me"],
-        queryFn: fetchMe,
-        staleTime: 1000 * 60, // 데이터 최신 유지 시간
-        retry: false, // 재시도
-    });
+  return useQuery<MeData | null>({
+    queryKey: ["me"],
+    queryFn: fetchMe,
+    staleTime: 1000 * 60, // 데이터 최신 유지 시간
+    retry: false, // 재시도
+  });
 };
 
 export default useMe;
